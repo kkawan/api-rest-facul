3347b844d62b0f2279ef64edffcbd22ddbeef585
Mariana Yumi Morishita Yokomizo <mariyumiyokomizo@gmail.com>
2025-11-24 10:58:02 -0300

    adiciona endpoints de atualização e remoção no ProductController

--- src/main/java/com/unifil/kawan/api_rest_facul/controller/ProductController.java
+++ src/main/java/com/unifil/kawan/api_rest_facul/controller/ProductController.java
@@ -30,4 +30,18 @@ public class ProductController {
     public Product findById(@PathVariable Long id) {
         return repo.findById(id).orElse(null);
     }
+
+     @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product dados) {
+        return repo.findById(id).map(u -> {
+            u.setNome(dados.getNome());
+            u.setPreco(dados.getPreco());
+            return repo.save(u);
+        }).orElse(null);
+    }
+
+      @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repo.deleteById(id);
+    }
 }
